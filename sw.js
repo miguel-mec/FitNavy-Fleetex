7e6d2705e8d982148165601818cbbad6a76c6d6b
miguel-mec <mmecgui@gmail.com>
2026-06-05 19:40:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,5 @@
-const CACHE_NAME = 'navyfit-final-v1';
-const ASSETS = ['index.html','manifest.json','icon-192.png','icon-512.png'];
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
-});
+const CACHE_NAME='navyfit-naval-v2';
+const ASSETS=['index.html','manifest.json','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
