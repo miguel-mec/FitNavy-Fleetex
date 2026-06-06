be9c6e1c6b4fcd0c7a93e686c10a50cf5bf9d783
miguel-mec <mmecgui@gmail.com>
2026-06-06 14:21:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='navyfit-v2-0-0';
+const CACHE_NAME='navyfit-v2-1';
 const ASSETS=['index.html','manifest.json','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
